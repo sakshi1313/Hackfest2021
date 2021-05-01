d821d4e0518399b014737d4c5cb9326c2e703c0c
Pranay-Pandey <pranaypandeyofficial@gmail.com>
2021-05-01 23:53:16 +0530

    Garbage Detector App

---
+++ app/src/main/java/org/tensorflow/lite/examples/detection/customview/ResultsView.java
@@ -0,0 +1,11 @@
+
+
+package org.tensorflow.lite.examples.detection.customview;
+
+import org.tensorflow.lite.examples.detection.tflite.Detector.Recognition;
+
+import java.util.List;
+
+public interface ResultsView {
+  public void setResults(final List<Recognition> results);
+}
